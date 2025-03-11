b9283ec32d619bdd09e97ebc23406d07d639dae6
Sander Descamps <sander_descamps@hotmail.com>
2025-03-11 08:53:50 +0100

    host perf draft

---
+++ collector/scraper/sensor_host_perf.go
@@ -0,0 +1,92 @@
+package scraper
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+type HostPerfSensor struct {
+	BaseSensor[types.ManagedObjectReference, mo.HostSystem]
+	Refreshable
+}
+
+func NewHostPerfSensor(scraper *VCenterScraper) *HostPerfSensor {
+	return &HostPerfSensor{
+		BaseSensor: BaseSensor[types.ManagedObjectReference, mo.HostSystem]{
+			cache:   make(map[types.ManagedObjectReference]*CacheItem[mo.HostSystem]),
+			scraper: scraper,
+			metrics: nil,
+		},
+	}
+}
+
+func (s *HostPerfSensor) Refresh(ctx context.Context, logger *slog.Logger) error {
+	sensorKind := reflect.TypeOf(s).String()
+	if hasLock := s.refreshLock.TryLock(); hasLock {
+		defer s.refreshLock.Unlock()
+		return s.unsafeRefresh(ctx, logger)
+	} else {
+		logger.Info("Sensor Refresh already running", "sensor_type", sensorKind)
+	}
+	return nil
+}
+
+func (s *HostPerfSensor) unsafeRefresh(ctx context.Context, logger *slog.Logger) error {
+	t1 := time.Now()
+
+	client, release, err := s.scraper.clientPool.Acquire()
+	defer release()
+	if err != nil {
+		return err
+	}
+	t2 := time.Now()
+	m := view.NewManager(client.Client)
+	v, err := m.CreateContainerView(
+		ctx,
+		client.ServiceContent.RootFolder,
+		[]string{"HostSystem"},
+		true,
+	)
+	if err != nil {
+		return err
+	}
+	defer v.Destroy(ctx)
+
+	var items []mo.HostSystem
+	err = v.Retrieve(
+		context.Background(),
+		[]string{"HostSystem"},
+		[]string{
+			"name",
+			"parent",
+			"summary",
+			"runtime",
+			"config.storageDevice",
+			"config.fileSystemVolume",
+			// "network",
+		},
+		&items,
+	)
+	t3 := time.Now()
+	s.setMetrics(&SensorMetric{
+		Name:           "host",
+		QueryTime:      t3.Sub(t2),
+		ClientWaitTime: t2.Sub(t1),
+		Status:         true,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, host := range items {
+		s.Update(host.Self, &host)
+	}
+
+	return nil
+}
---
+++ collector/scraper/sensor_perf.go
@@ -0,0 +1,4 @@
+package scraper
+
+type sensorPerfMetricBase struct {
+}
